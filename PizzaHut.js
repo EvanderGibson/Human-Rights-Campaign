d020df9a41a3ce2a7983097cbfc20bcc5a286f5d
piercefriden <piercefri@outlook.com>
2025-06-05 11:26:52 -0700

    my stuff

---
+++ PizzaHut.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+	$(".picL").click(function(){
+	  $(this).parent().prev().children().toggleClass("hidden");
+	  
+	});
+	$(".picR").click(function(){
+	  
+	  $(this).parent().next().children().toggleClass("hidden");
+	});
+	$(".question1").click(function(){
+	  
+	   $(".answer1").toggle();
+	});
+	$(".question2").click(function(){
+	  
+	   $(".answer2").toggle();
+	});
+	$(".question3").click(function(){
+	  
+	   $(".answer3").toggle();
+	});
+	$(".question4").click(function(){
+	  
+	   $(".answer4").toggle();
+	});   
+	$(".question5").click(function(){
+	  
+	   $(".answer5").toggle();
+	});   
+	$(".question6").click(function(){
+	  
+	   $(".answer6").toggle();
+	});
+	
+	$(".question7").click(function(){
+	  
+	   $(".answer7").toggle();
+	});
+	
+	$(".question8").click(function(){
+	  
+	   $(".answer8").toggle();
+	});
+	
+	$(".question9").click(function(){
+	  
+	   $(".answer9").toggle();
+	});
+	
+	$(".question10").click(function(){
+	  
+	   $(".answer10").toggle();
+	});
+	
+	$(".question11").click(function(){
+	  
+	   $(".answer11").toggle();
+	});
+	
+	$(".question12").click(function(){
+	  
+	   $(".answer12").toggle();
+	});
+	
+	$(".question13").click(function(){
+	  
+	   $(".answer13").toggle();
+	});
+	
+	$(".question14").click(function(){
+	  
+	   $(".answer14").toggle();
+	});
+	
+	$(".question15").click(function(){
+	  
+	   $(".answer15").toggle();
+	});
+});
